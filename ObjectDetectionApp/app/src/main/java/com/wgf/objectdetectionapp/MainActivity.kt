2304b0b9b053187fe8506cab1f7ef5defdbd5905
wgfactory <wgfactory01@gmail.com>
2020-10-22 16:05:08 +0900

    1022 Basic Object Detection

--- ObjectDetectionApp/app/src/main/java/com/wgf/objectdetectionapp/MainActivity.kt
+++ ObjectDetectionApp/app/src/main/java/com/wgf/objectdetectionapp/MainActivity.kt
@@ -5,16 +5,19 @@ import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
+import android.graphics.*
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Gravity
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -27,14 +30,15 @@ class MainActivity : AppCompatActivity() {
         Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
 
-    val FLAG_PERM_CAMERA = 98
-    val FLAG_PERM_STORAGE = 99
+    val REQ_PERMISSION_CAMERA = 98
+    val REQ_PERMISSION_STORAGE = 99
 
-    val FLAG_REQ_CAMERA = 101
-    val FLAG_REQ_STORAGE = 102
+    val ODT_REQ_CAMERA_IMAGE = 101
+    val ODT_REQ_GALLERY_IMAGE = 102
 
     // 카메라 원본이미지 Uri를 저장할 변수
-    var photoURI: Uri? = null
+    var mPhotoURI: Uri? = null
+    var mBitmap: Bitmap? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,8 +46,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // 3. 권한 체크하기 함수 호출!
-        if (checkPermission(STORAGE_PERMISSION, FLAG_PERM_STORAGE)) {
+        if (checkPermission(STORAGE_PERMISSION, REQ_PERMISSION_STORAGE)) {
             setViews()
+        }
+
+        //ODT 버튼 클릭시 객체 검출 시작!
+        buttonObjectDetection.setOnClickListener{
+
         }
     }
 
@@ -57,7 +66,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun openCamera() {
-        if (checkPermission(CAMERA_PERMISSION, FLAG_PERM_CAMERA)) {
+        if (checkPermission(CAMERA_PERMISSION, REQ_PERMISSION_CAMERA)) {
             dispatchTakePictureIntent()
         }
     }
@@ -65,23 +74,30 @@ class MainActivity : AppCompatActivity() {
     fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
-        startActivityForResult(intent, FLAG_REQ_STORAGE)
+        startActivityForResult(intent, ODT_REQ_GALLERY_IMAGE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK) {
             when(requestCode){
-                FLAG_REQ_CAMERA -> {
-                    if (photoURI != null) {
-                        val bitmap = loadBitmapFromMediaStoreBy(photoURI!!)
-                        imagePreview.setImageBitmap(bitmap)
-                        photoURI = null // 사용 후 null 처리
+                ODT_REQ_CAMERA_IMAGE -> {
+                    if (mPhotoURI != null) {
+                        val bitmap = loadBitmapFromMediaStoreBy(mPhotoURI!!)
+                        mBitmap = bitmap
+//                        imagePreview.setImageBitmap(bitmap)
+
+                        val image = getCapturedImage(mPhotoURI!!)
+                        runObjectDetection(image)
+
+                        mPhotoURI = null // 사용 후 null 처리
                     }
                 }
-                FLAG_REQ_STORAGE -> {
+                ODT_REQ_GALLERY_IMAGE -> {
                     val uri = data?.data
-                    imagePreview.setImageURI(uri)
+//                    imagePreview.setImageURI(uri)
+                    val image = getCapturedImage(uri!!)
+                    runObjectDetection(image)
                 }
             }
         }
@@ -105,7 +121,7 @@ class MainActivity : AppCompatActivity() {
         requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
         when (requestCode) {
-            FLAG_PERM_STORAGE -> {
+            REQ_PERMISSION_STORAGE -> {
                 for (grant in grantResults) {
                     if (grant != PackageManager.PERMISSION_GRANTED) {
                         Toast.makeText(this, "저장소 권한을 승인해야지만 앱을 사용할 수 있습니다.", Toast.LENGTH_LONG)
@@ -114,9 +130,10 @@ class MainActivity : AppCompatActivity() {
                         return
                     }
                 }
+
                 setViews()
             }
-            FLAG_PERM_CAMERA -> {
+            REQ_PERMISSION_CAMERA -> {
                 for (grant in grantResults) {
                     if (grant != PackageManager.PERMISSION_GRANTED) {
                         Toast.makeText(this, "카메라 권한을 승인해야지만 카메라를 사용할 수 있습니다.", Toast.LENGTH_LONG)
@@ -138,9 +155,9 @@ class MainActivity : AppCompatActivity() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
         createImageUri(newFileName(), "image/jpg")?.let { uri ->
-            photoURI = uri
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-            startActivityForResult(takePictureIntent, FLAG_REQ_CAMERA)
+            mPhotoURI = uri
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoURI)
+            startActivityForResult(takePictureIntent, ODT_REQ_CAMERA_IMAGE)
         }
     }
 
@@ -176,5 +193,76 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
         return image
+    }
+
+    /**
+     * getCapturedImage():
+     *     Decodes and center crops the captured image from camera.
+     */
+    private fun getCapturedImage(imgUri: Uri): Bitmap {
+
+        val srcImage = FirebaseVisionImage
+            .fromFilePath(baseContext, imgUri!!).bitmap
+
+        // crop image to match imageView's aspect ratio
+        val scaleFactor = Math.min(
+            srcImage.width / imagePreview.width.toFloat(),
+            srcImage.height / imagePreview.height.toFloat()
+        )
+
+        val deltaWidth = (srcImage.width - imagePreview.width * scaleFactor).toInt()
+        val deltaHeight = (srcImage.height - imagePreview.height * scaleFactor).toInt()
+
+        val scaledImage = Bitmap.createBitmap(
+            srcImage, deltaWidth / 2, deltaHeight / 2,
+            srcImage.width - deltaWidth, srcImage.height - deltaHeight
+        )
+        srcImage.recycle()
+        return scaledImage
+
+    }
+
+    /**
+     * MLKit Object Detection Function
+     */
+    private fun runObjectDetection(bitmap: Bitmap) {
+        // Step 1: create MLKit's VisionImage object
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+
+        // Step 2: acquire detector object
+        val options = FirebaseVisionObjectDetectorOptions.Builder()
+            .setDetectorMode(FirebaseVisionObjectDetectorOptions.SINGLE_IMAGE_MODE)
+            .enableMultipleObjects()
+            .enableClassification()
+            .build()
+        val detector = FirebaseVision.getInstance().getOnDeviceObjectDetector(options)
+
+        // Step 3: feed given image to detector and setup callback
+        detector.processImage(image)
+            .addOnSuccessListener {
+                // Task completed successfully
+                // Post-detection processing : draw result
+
+                if(it.size == 0) {
+                    showToast("아쉽지만 이미지에서 사물을 못찾았습니다. \n 다시 시도해주세요!")
+                }
+                val drawingView = DrawingView(applicationContext, it)
+                drawingView.draw(Canvas(bitmap))
+
+                runOnUiThread {
+                    imagePreview.setImageBitmap(bitmap)
+                }
+            }
+            .addOnFailureListener {
+                // Task failed with an exception
+                showToast("Oops, something went wrong!")
+            }
+    }
+
+    // 토스트 메세지 표시 하는 함수
+    private fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        val toast = Toast.makeText(applicationContext, message, duration)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 }
